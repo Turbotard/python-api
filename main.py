1f4ee0e77ca0619cbeeb1f53692c47927c66673c
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2023-09-18 20:09:22 +0200

    3 first userstories

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 import json
 
-with open('rdu-weather-history.json') as test :
-     data = json.load(test)
+with open('rdu-weather-history.json') as test:
+    data = json.load(test)
 print(data)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root(order: str = "asc"):
+    """
+
+    :param order:
+    :return:
+    """
+    sorted_data = sorted(data, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%d"), reverse=(order == "desc"))
+    return sorted_data
+
+@app.get("/prcp")
+async def read_root(order: str = "asc"):
+    """
+
+    :param order:
+    :return:
+    """
+    sorted_data = sorted(data, key=lambda x: x["prcp"], reverse=(order == "desc"))
+    return sorted_data
+
+
+
+
+
+
