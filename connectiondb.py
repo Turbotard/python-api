ba0df7f36d1f9ea9856fb84b2df257e780a31503
Turbotard <turbotard9@gmail.com>
2023-09-19 15:09:36 +0200

    connectiondb.py

--- connectiondb.py
+++ connectiondb.py
@@ -1,10 +1,11 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost:3306",
+    host="localhost",
     user="root",
     password="",
     database="python_api",
+    port="3306"
 )
 
 # faire quelque chose d'utile avec la connexion
