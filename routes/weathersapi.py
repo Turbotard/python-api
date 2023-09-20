ed623d8e681c9ec8f9fce18b36577fd3f3b6189e
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2023-09-20 09:17:43 +0200

    id

--- routes/weathersapi.py
+++ routes/weathersapi.py
@@ -3,9 +3,12 @@ import random
 
 with open('../rdu-weather-history.json', 'r') as file:
     data = json.load(file)
-
+i = 1
 for item in data:
+    item["id"] = i
     item["idcities"] = random.randint(0, 6)
+    i += 1
+
 
 with open('rdu-weather-history.json', 'w') as file:
     json.dump(data, file, indent=4)
