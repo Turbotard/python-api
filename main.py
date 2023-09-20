1ca19a90b379b6b7ef57a6e0ec34cc0eb042a171
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2023-09-20 12:21:49 +0200

    country name

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from routes.weathers import temperature
+from routes.countries import name
 
 app = FastAPI()
 
-
+countries_name_router = name.countries_name_router
 temperature_router = temperature.temperature_router
 
 
-
+app.include_router(countries_name_router)
 app.include_router(temperature_router)
 
 
---
+++ routes/countries/name.py
@@ -0,0 +1,33 @@
+# temperature.py
+
+from fastapi import APIRouter, HTTPException
+from shared import request_counts
+from connectiondb import get_database_connection
+
+countries_name_router = APIRouter()
+
+@countries_name_router.get("/countries/{name}")
+def filter_by_temperature(name: str):
+    try:
+
+        db = get_database_connection()
+        cursor = db.cursor()
+
+        # Exécutez une requête SQL pour récupérer les données filtrées
+        query = f"SELECT * FROM countries WHERE names = %s"
+        cursor.execute(query, (name,))
+
+        # Récupérez les données de la base de données
+        data = cursor.fetchone()
+
+        # Fermez la connexion à la base de données
+        cursor.close()
+        db.close()
+
+        # Transformez les résultats en un format approprié (par exemple, liste de dictionnaires)
+        formatted_data = [{'id': data[0], 'names': data[1]}]
+
+        return { "filtered_data": formatted_data}
+    except Exception as e:
+        error_message = f"Erreur lors du filtrage par nom : {str(e)}"
+        raise HTTPException(status_code=422, detail=error_message)
