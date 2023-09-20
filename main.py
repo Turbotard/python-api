472bb9029da45447623e491377e140ddb4722bc5
Turbotard <turbotard9@gmail.com>
2023-09-20 15:00:59 +0200

    big update

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
+from routes.cities import weathers_city
+from routes.cities import name, add
 from routes.weathers import temperature
 
 app = FastAPI()
 
-
+weathers_router = weathers_city.weathers_router
+cities_router = name.cities_router
+citiesadd_router = add.cities_router
 temperature_router = temperature.temperature_router
 
-
-
 app.include_router(temperature_router)
-
-
-
-
-
-
+app.include_router(cities_router)
+app.include_router(citiesadd_router)
+app.include_router(weathers_router)
