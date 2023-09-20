2732d6d633ab8b16829f059de0555fbfb0edd18c
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2023-09-20 09:42:55 +0200

    fix api

--- routes/citiesapi.py
+++ routes/citiesapi.py
@@ -2,13 +2,13 @@ import json
 
 # Existing city data
 cities = [
-    {"id": 1, "idcountries": 1, "name": "Paris"},
-    {"id": 2, "idcountries": 2, "name": "New York"},
-    {"id": 3, "idcountries": 1, "name": "Marseille"},
-    {"id": 4, "idcountries": 3, "name": "Toronto"},
-    {"id": 5, "idcountries": 1, "name": "Lyon"},
-    {"id": 6, "idcountries": 2, "name": "Los Angeles"},
-    {"id": 7, "idcountries": 3, "name": "Vancouver"}
+    {"id": 1, "id_country": 1, "name": "Paris"},
+    {"id": 2, "id_country": 2, "name": "New York"},
+    {"id": 3, "id_country": 1, "name": "Marseille"},
+    {"id": 4, "id_country": 3, "name": "Toronto"},
+    {"id": 5, "id_country": 1, "name": "Lyon"},
+    {"id": 6, "id_country": 2, "name": "Los Angeles"},
+    {"id": 7, "id_country": 3, "name": "Vancouver"}
 ]
 
 # Save the updated data to a JSON file
--- routes/weathersapi.py
+++ routes/weathersapi.py
@@ -8,10 +8,10 @@ with open('../rdu-weather-history.json', 'r') as file:
 # 2. Pour chaque élément, ajoutez un id, puis réorganisez les clés
 for index, item in enumerate(data):
     item["id"] = index + 1  # Génération d'un identifiant unique
-    item["idcities"] = random.randint(0, 6)
+    item["id_city"] = random.randint(0, 6)
 
     # Réorganisation des clés
-    keys_order = ['id', 'idcities'] + [key for key in item if key not in ['id', 'idcities']]
+    keys_order = ['id', 'id_city'] + [key for key in item if key not in ['id', 'id_city']]
     data[index] = {key: item[key] for key in keys_order}
 
 # 3. Sauvegardez le fichier JSON modifié
