f5f7eb130c3993c99fbbe59f242f2b3687a92d28
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-09-21 10:06:49 +0200

    main

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
-from routes.cities import city_name, city_add, city_weathers, city_delete, city_update
-from routes.weathers import temperature
-
+from routes.weathers import weather_add, weather_data, weather_date, weather_delete, weather_precipitation, \
+    weather_temperature, weather_update, weather_statistics
+from routes import root
 app = FastAPI()
 
-weathers_router = weathers_city.weathers_router
-cities_router = name.cities_router
-cities_add_router = add.cities_router
-cities_delete_router = delete.cities_router
-cities_update_router = update.cities_router
+weathers_add_router = weather_add.weathers_add_router
+weathers_data_router = weather_data.weathers_data_router
+weathers_date_router = weather_date.weathers_date_router
+weathers_delete_router = weather_delete.weathers_delete_router
+weathers_precipitation_router = weather_precipitation.weathers_precipitation_router
+weathers_temperature_router = weather_temperature.weathers_temperature_router
+weathers_update_router = weather_update.weathers_update_router
+weather_root_router = root.weather_root_router
+weather_statistics_router = weather_statistics.weather_statistics_router
 
-app.include_router(cities_router)
-app.include_router(cities_add_router)
-app.include_router(weathers_router)
-app.include_router(cities_delete_router)
-app.include_router(cities_update_router)
+app.include_router(weathers_add_router)
+app.include_router(weathers_data_router)
+app.include_router(weathers_date_router)
+app.include_router(weathers_delete_router)
+app.include_router(weathers_precipitation_router)
+app.include_router(weathers_temperature_router)
+app.include_router(weathers_update_router)
+app.include_router(weather_root_router)
+app.include_router(weather_statistics_router)
