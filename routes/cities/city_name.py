bfa510ca932b3c86d0a7dcd62a0abff00e0d6050
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-09-21 12:50:06 +0200

    fix all !

--- routes/cities/city_name.py
+++ routes/cities/city_name.py
@@ -3,15 +3,15 @@ from connectiondb import get_database_connection
 
 cities_name_router = APIRouter()
 
-@cities_name_router.get("/countries/cities/{city_name}")
-def get_city_by_name(city_name: str):
+@cities_name_router.get("/countries/cities/name/{name}")
+def get_city_by_name(name: str):
     try:
         db = get_database_connection()
         cursor = db.cursor()
 
         # Utilisation de requêtes paramétrées pour éviter les injections SQL
         query = "SELECT * FROM cities WHERE name = %s"
-        cursor.execute(query, (city_name,))
+        cursor.execute(query, (name,))
 
         data = cursor.fetchone()  # Récupère une seule entrée puisqu'on s'attend à ce qu'un nom de ville soit unique
 
@@ -19,7 +19,7 @@ def get_city_by_name(city_name: str):
         db.close()
 
         if not data:
-            raise HTTPException(status_code=404, detail=f"Aucune ville nommée {city_name} trouvée.")
+            raise HTTPException(status_code=404, detail=f"Aucune ville nommée {name} trouvée.")
 
         city_data = {'Code City': data[1], 'Name': data[3]}
 
