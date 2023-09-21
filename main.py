099595ef87804fdbd55985f6a05f39891fe70eba
Turbotard <turbotard9@gmail.com>
2023-09-21 10:01:29 +0200

    update put and delete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes.cities import name, add, weathers_city
+from routes.cities import name, add, weathers_city, delete, update
 from routes.weathers import temperature
 
 app = FastAPI()
@@ -7,7 +7,11 @@ app = FastAPI()
 weathers_router = weathers_city.weathers_router
 cities_router = name.cities_router
 cities_add_router = add.cities_router
+cities_delete_router = delete.cities_router
+cities_update_router = update.cities_router
 
 app.include_router(cities_router)
 app.include_router(cities_add_router)
 app.include_router(weathers_router)
+app.include_router(cities_delete_router)
+app.include_router(cities_update_router)
