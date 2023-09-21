9e2b5d3a438d348c3540c62d73030d145b14f6c9
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-09-21 14:13:40 +0200

    doc weathers_delete

--- routes/weathers/weather_delete.py
+++ routes/weathers/weather_delete.py
@@ -6,6 +6,19 @@ weathers_delete_router = APIRouter()
 
 @weathers_delete_router.delete("/countries/cities/weathers/{name_city}/{date_to_delete}")
 def delete_entry(name_city: str, date_to_delete: str):
+    """
+    Supprime une entrée de données météorologiques par date pour une ville donnée.
+
+    Args:
+        name_city (str): Le nom de la ville associée à l'entrée météorologique.
+        date_to_delete (str): La date de l'entrée à supprimer au format 'YYYY-MM-DD'.
+
+    Returns:
+        dict: Un dictionnaire contenant le nombre de requêtes traitées et un message de confirmation.
+
+    Raises:
+        HTTPException: Si une erreur survient lors de la suppression de l'entrée ou si la ville n'est pas trouvée.
+    """
     try:
         weathers_request_counts['delete_entry'] += 1
 
