6b801d912f6d2005cf0afcce92dc2095b3c9ec76
Turbotard <turbotard9@gmail.com>
2023-09-21 10:08:51 +0200

    update main

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from routes.weathers import weather_add, weather_data, weather_date, weather_delete, weather_precipitation, \
     weather_temperature, weather_update, weather_statistics
+from routes.cities import city_name, city_add, city_weathers, city_delete, city_update
+
 from routes import root
 app = FastAPI()
+
+
+weathers_router = city_weathers.weathers_router
+cities_router = city_name.cities_router
+cities_add_router = city_add.cities_router
+cities_delete_router = city_delete.cities_router
+cities_update_router = city_update.cities_router
+
+app.include_router(cities_router)
+app.include_router(cities_add_router)
+app.include_router(weathers_router)
+app.include_router(cities_delete_router)
+app.include_router(cities_update_router)
 
 weathers_add_router = weather_add.weathers_add_router
 weathers_data_router = weather_data.weathers_data_router
